ba6cdc39fcbd996fb91eb77ef4cf4edbb3abce4b
Will <wlauer00@gmail.com>
2021-06-09 14:53:43 -0500

    Added Classes

--- src/dungeon/Item.java
+++ src/dungeon/Item.java
@@ -2,13 +2,15 @@ package dungeon;
 
 public abstract class Item {
     private String type;
+    private String name;
 
-
-    public Item(String type) {
+    public Item(String type, String name) {
         this.type = type;
+        this.name = name;
     }
 
     public void use() {
-
+        System.out.println("You used your " + name + "!");
     }
+
 }
