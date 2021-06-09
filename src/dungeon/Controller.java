2177f91e86e670ed285df4fe793b1b6907f355ae
Will <wlauer00@gmail.com>
2021-06-09 14:45:32 -0500

    Added some classes

--- src/dungeon/Controller.java
+++ src/dungeon/Controller.java
@@ -1,11 +1,10 @@
-package sample;
+package dungeon;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.text.TextFlow;
 
 import java.io.File;
 
---
+++ src/dungeon/Item.java
@@ -0,0 +1,14 @@
+package dungeon;
+
+public abstract class Item {
+    private String type;
+
+
+    public Item(String type) {
+        this.type = type;
+    }
+
+    public void use() {
+
+    }
+}
