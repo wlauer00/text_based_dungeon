9e7acbf3d35925d57bda14b8e53ac05bd6088bad
Will <wlauer00@gmail.com>
2021-06-09 12:22:49 -0500

    HMMMM

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,23 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
+
+
 public class Main extends Application {
+
+    @FXML
+    public Button importDungeon;
+
+    @FXML
+    public TextFlow mainText;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
