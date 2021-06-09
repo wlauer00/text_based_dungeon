f135d832d4dfb985c1781886daf2acf597aac656
Will <wlauer00@gmail.com>
2021-06-09 12:39:15 -0500

    Did some work

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,19 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.TextFlow;
 
 public class Controller {
     public ImageView playerImage;
-    public TextFlow mainText;
+    public TextField mainText;
     public ImageView enemyImage;
     public Button importDungeon;
 
-
+    @FXML
+    private void importPressed() {
+        mainText.setText("YOU PRESSED THE BUTTON!");
+    }
 }
