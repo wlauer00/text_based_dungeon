c278bc3703ce93a2872a6691500b05be327c9e83
Will <wlauer00@gmail.com>
2021-06-09 12:29:48 -0500

    Changed up FXML

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.TextFlow;
+
 public class Controller {
+    public ImageView playerImage;
+    public TextFlow mainText;
+    public ImageView enemyImage;
+    public Button importDungeon;
+
+
 }
