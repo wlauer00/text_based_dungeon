83d5f8b795c6c96cfb2a5c05c5437697c1c57e11
Will <wlauer00@gmail.com>
2021-06-09 12:55:07 -0500

    Changed Hero Pic

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,8 +3,11 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.TextFlow;
+
+import java.io.File;
 
 public class Controller {
     public ImageView playerImage;
@@ -12,8 +15,13 @@ public class Controller {
     public ImageView enemyImage;
     public Button importDungeon;
 
+
     @FXML
     private void importPressed() {
         mainText.setText("YOU PRESSED THE BUTTON!");
+
+        File file = new File("newHero.PNG");
+        Image heroImage = new Image(file.toURI().toString());
+        playerImage.setImage(heroImage);
     }
 }
